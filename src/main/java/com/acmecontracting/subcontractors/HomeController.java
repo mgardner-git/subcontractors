faa5c5a0ba73ecac325b298ff52252fc027e342b
mgardner-git <monte@gardnerweb.org>
2015-11-03 21:48:50 -0700

    Getting this to work on my computer at home.

--- src/main/java/com/acmecontracting/subcontractors/HomeController.java
+++ src/main/java/com/acmecontracting/subcontractors/HomeController.java
@@ -6,15 +6,10 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.acmecontracting.subcontractors.project.Project;
 
 /**
  * Handles requests for the application home page.
@@ -40,7 +35,5 @@ public class HomeController {
 		
 		return "home";
 	}
-	
-
 	
 }
